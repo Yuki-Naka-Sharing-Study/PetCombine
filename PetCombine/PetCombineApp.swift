f888ab2d6ce22135ad1c8080789da4e6a899eb3e
仲優樹 <yu-ki216m@outlook.jp>
2025-06-26 14:49:13 +0900

    Initial Commit

---
+++ PetCombine/PetCombineApp.swift
@@ -0,0 +1,17 @@
+//
+//  PetCombineApp.swift
+//  PetCombine
+//
+//  Created by 仲優樹 on 2025/06/26.
+//
+
+import SwiftUI
+
+@main
+struct PetCombineApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
